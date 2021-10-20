ea53ba3fbac328b5968a288ce5a82ed0a23cde6b
AzizbekSaidkarimov2008 <86466353+AzizbekSaidkarimov2008@users.noreply.github.com>
2021-10-20 15:56:42 +0500

    Admin Panel Second Lesson

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
 const createError = require("http-errors");
 const express = require("express");
-const exhbs = require('express-handlebars')
+const exhbs = require("express-handlebars");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const adminRouter = require("./routes/admin");
 
 const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-app.set(exhbs({
-  layoutsDir: path.join(__dirname, "views"),
-  defaultLayout: 'main',
-  extname: 'hbs',
-  partialsDir: [
-    path.join(__dirname, "views/partials")
-  ],
-  runtimeOptions: {
-    allowProtoMethodsByDefault: true,
-    allowProtoPropertiesByDefault: true
-  }
-}))
+app.engine(
+  "hbs",
+  exhbs({
+    layoutsDir: path.join(__dirname, "views/layouts"),
+    defaultLayout: "layout",
+    extname: "hbs",
+    partialsDir: [path.join(__dirname, "views/partials")],
+    runtimeOptions: {
+      allowProtoMethodsByDefault: true,
+      allowProtoPropertiesByDefault: true,
+    },
+  })
+);
 
-require('./helper/db')()
+require("./helper/db")();
 
 // const pass = 'family1225vsburxon'
 
@@ -37,7 +38,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/admin", adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
